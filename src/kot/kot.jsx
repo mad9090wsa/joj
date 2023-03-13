9d3336d24750265db6352ab2dd190048884c7531
mad90990 <abdulaziz.a.a.2006@gmail.com>
2023-03-13 22:08:21 +0500

    sdnnnnniaooooooo2-kun

--- src/kot/kot.jsx
+++ src/kot/kot.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import kot from './kot.module.css';
 import img from "./Group 900.png";
-
+import img2 from "./Group 1457.png"
 
 function Kot() {
 
@@ -9,9 +9,12 @@ function Kot() {
     <div className={kot.ada}>
        <div className={kot.container}>
           <div className={kot.text__ada2}>
-            ddddddddddddddddddddddddddddddddddddddddddddd          
+            <div className={kot.text__sub__text}>The best video online meeting software.</div>   
+            <div className={kot.text__sub__text_2}>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default</div> 
+            <button className={kot.bnt__sub}>Start for free</button>   
           </div>
            <img src={img} alt="#" className={kot.img__ada2} />
+           <img src={img2} alt="#" className={kot.img__ada3} />
        </div>
     </div>
   );
