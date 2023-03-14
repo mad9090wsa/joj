d8ee9c260efcdb9ff556f45be112c86a888c9808
mad90990 <abdulaziz.a.a.2006@gmail.com>
2023-03-14 22:09:34 +0500

    3-kun2-qism

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from "react";
 import Add from "./munu.jsx/munu";
 import Kot from "./kot/kot";
 import Ikki from "./ikki/ikki";
-
+import Rasm from "./rasm/rasm";
 function App() {
   return(
     <>
     <Add/>
     <Kot/>
     <Ikki/>
+    <Rasm/>
     </>
   )
   
---
+++ src/rasm/rasm.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import rams from "./rasm.module.css";
+import img from "./img/Airbnb Logo.png";
+import img1 from "./img/Hubspot Logo.png";
+import img2 from "./img/Google Logo.png";
+
+let Rasm = ()=>{
+   return(
+    <div className={rams.ada}>
+        <div className={rams.container}>
+            <a href="https://www.airbnb.ru/"><img src={img} alt="#" /></a>
+            <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjzgIj789v9AhWCBaIDHV0LCZIYABACGgJsZQ&ohost=www.google.com&cid=CAESbeD2bzCokUWKqEm6UXKN__3rBW7no-rbF4OphHBVCEGOnr2ZmLKzl1Rl8TFyaYG39CcYlTEYsr3JXu-aMd_huroCNsMDCCemh1zCrKNVFjsPnxowFhC62YSfXrgD4Qy2cH9eglhP0oJW6b6MJzM&sig=AOD64_1v-CVJ3CKTRMC4SLqOxVv7JNx4WA&q&adurl&ved=2ahUKEwiq34H789v9AhUJqIsKHR8FBrgQ0Qx6BAgKEAE"><img src={img1} alt="#" /> </a>
+            <a href="https://www.google.ru/"><img src={img2} alt="#" /></a>
+            <a href="">sssssssssss</a>
+            <a href="">sssssssssss</a>
+            <a href="">sssssssssss</a>
+        </div>
+    </div>
+   )
+};
+
+export default Rasm;
