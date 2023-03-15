f9ddea2558324d869f1906073d71dd3a0a392a82
mad90990 <abdulaziz.a.a.2006@gmail.com>
2023-03-15 20:12:58 +0500

    4-kun2012

--- src/rasm/rasm.jsx
+++ src/rasm/rasm.jsx
@@ -12,12 +12,12 @@ let Rasm = ()=>{
    return(
     <div className={rams.ada}>
         <div className={rams.container}>
-            <a href="https://www.airbnb.ru/"><img src={img} alt="#" /></a>
-            <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjzgIj789v9AhWCBaIDHV0LCZIYABACGgJsZQ&ohost=www.google.com&cid=CAESbeD2bzCokUWKqEm6UXKN__3rBW7no-rbF4OphHBVCEGOnr2ZmLKzl1Rl8TFyaYG39CcYlTEYsr3JXu-aMd_huroCNsMDCCemh1zCrKNVFjsPnxowFhC62YSfXrgD4Qy2cH9eglhP0oJW6b6MJzM&sig=AOD64_1v-CVJ3CKTRMC4SLqOxVv7JNx4WA&q&adurl&ved=2ahUKEwiq34H789v9AhUJqIsKHR8FBrgQ0Qx6BAgKEAE"><img src={img1} alt="#" /> </a>
-            <a href="https://www.google.ru/"><img  src={img2} alt="#" /></a>
-            <a href="./#"><img src={img3} alt="#" /></a>
-            <a href="./#"><img className={rams.kotrasm} src={img4} alt="#" /></a>
-            <a href="./#"><img className={rams.kotrasm} src={img5} alt="#" /></a>
+            <a href="https://www.airbnb.ru/"><img className={rams.kkk} src={img} alt="#" /></a>
+            <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjzgIj789v9AhWCBaIDHV0LCZIYABACGgJsZQ&ohost=www.google.com&cid=CAESbeD2bzCokUWKqEm6UXKN__3rBW7no-rbF4OphHBVCEGOnr2ZmLKzl1Rl8TFyaYG39CcYlTEYsr3JXu-aMd_huroCNsMDCCemh1zCrKNVFjsPnxowFhC62YSfXrgD4Qy2cH9eglhP0oJW6b6MJzM&sig=AOD64_1v-CVJ3CKTRMC4SLqOxVv7JNx4WA&q&adurl&ved=2ahUKEwiq34H789v9AhUJqIsKHR8FBrgQ0Qx6BAgKEAE"><img className={rams.kkk} src={img1} alt="#" /> </a>
+            <a href="https://www.google.ru/"><img className={rams.kkk} src={img2} alt="#" /></a>
+            <a href="./#"><img className={rams.kkkk} src={img3} alt="#" /></a>
+            <a href="./#"><img className={rams.kkk} src={img4} alt="#" /></a>
+            <a href="./#"><img className={rams.kkk} src={img5} alt="#" /></a>
         </div>
     </div>
    )
