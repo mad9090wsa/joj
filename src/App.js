37163052a632dcdd0c291fcd2ccfc3a1894a82b6
mad90990 <abdulaziz.a.a.2006@gmail.com>
2023-03-16 15:51:14 +0500

    5-kun1-qism

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     <Text/>
 {/* sssssssssssss */}
 <Click/>
+ffffff
     </>
   )
   
--- src/click/click.jsx
+++ src/click/click.jsx
@@ -1,27 +1,63 @@
-import "./click.css";
 import React,{useState} from "react";
-
+import click from "./click.module.css";
+import img from "./Group 866.png";
 
 function Click() {
   const [nav,setNav]= useState(false);
   const [nav2,setNav2]= useState(true);
+  let a1 = ()=>{
+    setNav(!nav);
+    setNav2(!nav2);
+  };
+  let a2 = ()=>{
+    setNav(!nav);
+    setNav2(!nav2);
+  };
   return (
-
-
-
-<header>
-  
-<div onClick={()=> {setNav(!nav);console.log(`${nav}ii ${nav2}`);setNav2(!nav2);}}>
-    click
+<header className={click.ada}>
+  <div className={click.container}>
+<div className={click.ada__text}>
+  <div className={click.subtext1}>Flexible pricing  plan</div>
+  <div className={click.subtext2}>We are familiar with offline speed dating flow so we designed out online
+platform considering all the requirements</div>
 </div>
 
 
-{/* <ul className={nav ? [sss.munu, sss.aaa].join(' '):[sss.munu]} ></ul> */}
+ <div className={click.ku}>
+   <div  onClick={a1} className={nav ? [click.d1, click.bac].join(' '):[click.d1]}> Annually</div>
+   <div  onClick={a2} className={nav2 ? [click.d2, click.bac].join(' '):[click.d2]}>Monthly</div>
+   
+ </div>
 
+<div className={click.cart_ada}>
+  <div className={click.cart}>
+          <div className={click.sub_cart}>d</div>
+          <div className={click.sub_cart_list}>s</div>
+  </div>
+  {/* ----------- */}
+  <div className={click.cart}>
+            <div className={click.sub_cart}>a</div>
+            <div className={click.sub_cart_list}>
+              <ul className={click.list__ul}>
+                <li> <img src={img} alt="#" /> ddddddd</li>
+                <li> <img src={img} alt="#" /> ddddddd</li>
+                <li> <img src={img} alt="#" /> ddddddd</li>
+                <li> <img src={img} alt="#" /> ddddddd</li>
+                <li> <img src={img} alt="#" /> ddddddd</li>
+                <li> <img src={img} alt="#" /> ddddddd</li>
+                <li> <img src={img} alt="#" /> ddddddd</li>
+              </ul>
+            </div>
+            
+    </div>
+      {/* ----------- */}
+    <div className={click.cart}>
+           <div className={click.sub_cart}>s</div>
+           <div className={click.sub_cart_list}>d</div>
+    </div>
+</div>
 
-
-
-
+ </div>
 </header>
   );
 }
