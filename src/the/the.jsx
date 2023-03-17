83339cf8393de050330fa7e279c59a902295f2ae
mad90990 <abdulaziz.a.a.2006@gmail.com>
2023-03-17 18:05:06 +0500

    6-kun2-qism2

--- src/the/the.jsx
+++ src/the/the.jsx
@@ -16,10 +16,47 @@ function The() {
               <div className={the.ul_loga}>
                 <div>Resources</div>
                 <ul>
-                    liffff
+                    <li>How it works</li>
+                    <li>Login</li>
+                    <li>Task</li>
+                    <li>Blog</li>
+                    <li>About</li>
+                </ul>
+              </div>
+              <div className={the.ul_loga}>
+                <div>Product</div>
+                <ul>
+                    <li>Downlaod apps</li>
+                    <li>Help center</li>
+                    <li>Blog</li>
+                    <li>Caeers</li>
+                    <li>Partners</li>
+                </ul>
+              </div>
+              <div className={the.ul_loga}>
+                <div>More</div>
+                <ul>
+                    <li>Demo</li>
+                    <li>Price</li>
+                    <li>Business</li>
+                    <li>pro</li>
+                    <li>About</li>
+                </ul>
+              </div>
+              <div className={the.ulsub}>
+                <div>Contact</div>
+                <ul>
+                    <li>Email: admin@spaceit.com</li>
+                    <li>phone: 01234 567890</li>
+                    <li>Location: Amsterdam Netherlands</li>
+    
                 </ul>
               </div>
             </div>
+           <div className={the.thesub}>
+               <div className={the.hr}></div>
+                  <div className={the.subtext}>Copyright By@QuomodoTheme - 2021</div>
+           </div>
         </div>
     )
 }
