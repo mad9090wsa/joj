b8b2102fbd2f36c527bf16a5d6d79a39edfb2be5
mad90990 <abdulaziz.a.a.2006@gmail.com>
2023-03-17 15:30:16 +0500

    6-kun

--- src/kub/kub.jsx
+++ src/kub/kub.jsx
@@ -4,18 +4,35 @@ import rasm from "./img/Ellipse 177.png";
 import rasm1 from "./img/Ellipse 178.png";
 import rasm2 from "./img/Ellipse 179.png";
 import rasm3 from "./img/Ellipse 180.png";
-import rasm4 from "./img/Ellipse 180.png";
+import img1 from "./img2/Ellipse 181.png";
+import img2 from "./img2/Ellipse 182.png";
+import img3 from "./img2/Ellipse 183.png";
+import img4 from "./img2/Ellipse 184.png";
 
 function Kub() {
     return(
         <div className={kit.ada}>
             <div className={kit.container}>
+                <div className={kit.text}>
+                    <div className={kit.sub__text}>Testimonial</div>
+                    <div className={kit.sub__text2}>
+                        <div className={kit.text2222}>Here are many variations of passag Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words don't look even slightly believable.</div>
+                        <div className={kit.subsubtext}>Cris Gayle <div>Manager</div></div>  
+                        
+                    </div>
+                </div>
                  <div className={kit.rasma}>
                      <img src={rasm} alt="#" />
                      <img src={rasm1} alt="#" />
                      <img src={rasm2} alt="#" />
                      <img src={rasm3} alt="#" />
+                    <div className={kit.rasm__sub}>
+                        <img src={img1} alt="#" />
+                        <img src={img2} alt="#" />
+                        <img src={img3} alt="#" />
+                        <img src={img4} alt="#" />
 
+                    </div>
                  </div>
             </div>
         </div>
